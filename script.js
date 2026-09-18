4c9b6054d0b16fd16a1e0c8bbd3d471ed5f9998e
Joshua Almario <joshua122204@gmail.com>
2026-09-18 12:27:17 -0700

    added transition

--- script.js
+++ script.js
@@ -12,7 +12,23 @@ $(function() {
     );                  
 });
 
-$(document).ready(function() {
-    $("#header").load("header.html");
-    $("#footer").load("footer.html");
-});
+function updateView(event) {
+  // Handle the difference in whether the event is fired on the <a> or the <img>
+  const targetIdentifier = event.target.firstChild || event.target;
+
+  const displayNewImage = () => {
+    const mainSrc = `${targetIdentifier.src.split("_th.jpg")[0]}.jpg`;
+    galleryImg.src = mainSrc;
+    galleryCaption.textContent = targetIdentifier.alt;
+  };
+
+  // Fallback for browsers that don't support View Transitions:
+  if (!document.startViewTransition) {
+    displayNewImage();
+    return;
+  }
+
+  // With View Transitions:
+  const transition = document.startViewTransition(() => displayNewImage());
+}
+
